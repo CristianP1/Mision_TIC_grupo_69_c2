cc840b9a223bea3c1ac50a6836ab3115c31b8945
Cristianp1 <perezcristian730@gmail.com>
2021-08-01 18:42:42 -0500

    inicio ciclo 2 grupo 69

---
+++ clase02/src/main/java/co/edu/utp/misiontic2022/c2/App.java
@@ -0,0 +1,18 @@
+package co.edu.utp.misiontic2022.c2;
+
+import javax.sound.sampled.SourceDataLine;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        
+
+    }
+
+
+}
